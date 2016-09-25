e3a3eb6b0de76ce9d388cb21d38ff1261c83269d
Ilan Khirin <ilan.khirin@gmail.com>
2016-09-25 15:31:59 +0300

    updated main page controller

--- app/pages/main-page/main-page-controller.js
+++ app/pages/main-page/main-page-controller.js
@@ -1,12 +1,33 @@
-(function() {
+(function () {
   angular.module('summarySharing').controller('MainPageCtrl', ['$scope', MainController])
 
   function MainController($scope) {
     var vm = this;
+
     vm.categories = getCategories();
+    vm.summaries = getSummaries();
 
     function getCategories() {
-      // $http.get()
+      //return getData('');
+      return ['Math', 'Science', 'History'];
+    }
+
+    function getSummaries() {
+
+      // getData('');
+      return [
+        {
+          title: "World War II Summary",
+          author: "Adolf Hitler",
+          likes: 5,
+          dislikes: 3,
+          content: "asdasdasdasdasdasdasd"
+        }
+      ];
+    }
+
+    function getData(url) {
+      // $http.get('')
       //   .then(function (response) {
       //       return response.data;
       //   },
@@ -14,7 +35,6 @@
       //       console.log('error');
       //       console.log(response)
       //   });
-      return ['Math', 'Science', 'History'];
     }
   }
 })();
