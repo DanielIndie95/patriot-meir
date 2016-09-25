63da762627d78b7b1eeef6a67c1940b9cd3ee8fc
DESKTOP-CG49VP0\DanielEdry <danielindie95@gmail.com>
2016-09-25 12:11:49 +0300

    good luck

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name meirChkaApp
+ * @description
+ * # meirChkaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('meirChkaApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meirChkaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the meirChkaApp
+ */
+angular.module('meirChkaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
