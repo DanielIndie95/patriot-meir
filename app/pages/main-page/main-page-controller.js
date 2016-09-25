57a3bd86225405edcaa2a6e2d546852096b631aa
DESKTOP-CG49VP0\DanielEdry <danielindie95@gmail.com>
2016-09-25 15:44:56 +0300

    made view-summary panels , added ng-meterial

--- app/pages/main-page/main-page-controller.js
+++ app/pages/main-page/main-page-controller.js
@@ -1,7 +1,40 @@
-(function() {
+(function () {
   angular.module('summarySharing').controller('MainPageCtrl', ['$scope', MainController])
 
   function MainController($scope) {
     var vm = this;
+
+    vm.categories = getCategories();
+    vm.summaries = getSummaries();
+
+    function getCategories() {
+      //return getData('');
+      return ['Math', 'Science', 'History'];
+    }
+
+    function getSummaries() {
+
+      // getData('');
+      return [
+        {
+          title: "World War II Summary",
+          author: "Adolf Hitler",
+          likes: 5,
+          dislikes: 3,
+          content: "asdasdasdasdasdasdasd"
+        }
+      ];
+    }
+
+    function getData(url) {
+      // $http.get('')
+      //   .then(function (response) {
+      //       return response.data;
+      //   },
+      //   function (response) {
+      //       console.log('error');
+      //       console.log(response)
+      //   });
+    }
   }
 })();
