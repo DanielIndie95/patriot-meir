cfd0dea2f30438119859888a18356c50ce13f8cf
Meir Blachman <meirblachman@protonmail.com>
2016-09-25 17:13:23 +0300

    added template for summary questions

---
+++ app/components/star-rating/star-rating.directive.js
@@ -0,0 +1,47 @@
+(function () {
+  angular.module('summarySharing')
+    .directive('starRating', starRating);
+
+  function starRating() {
+    return {
+      restrict: 'E',
+      templateUrl: 'components/star-rating/star-rating.template.html',
+      scope: {
+        rating: '=',
+        onRatingSelect: '&?',
+        readonly: '=?'
+      },
+      link: function (scope, element, attributes) {
+        scope.max = 10;
+
+        if (scope.isOpen == undefined) {
+          scope.isOpen = false;
+        }
+
+        scope.toggle = function (index) {
+          if (scope.readonly == undefined || scope.readonly === false) {
+            scope.rating.value = index + 1;
+            scope.onRatingSelect({
+              'rating.value': index + 1
+            });
+          }
+        };
+        scope.$watch('rating.value', function (oldValue, newValue) {
+          if (newValue) {
+            updateStars();
+          }
+        });
+
+        function updateStars() {
+          scope.stars = [];
+          for (var i = 0; i < scope.max; i++) {
+            scope.stars.push({
+              filled: i < scope.rating.value
+            });
+          }
+        }
+      }
+    };
+  }
+
+})();
