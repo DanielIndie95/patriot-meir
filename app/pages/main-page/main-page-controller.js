af1ae639bf88010ef90595df86244ee10357fa76
Ilan Khirin <ilan.khirin@gmail.com>
2016-09-25 15:21:00 +0300

    added categories to main page

--- app/pages/main-page/main-page-controller.js
+++ app/pages/main-page/main-page-controller.js
@@ -3,5 +3,18 @@
 
   function MainController($scope) {
     var vm = this;
+    vm.categories = getCategories();
+
+    function getCategories() {
+      // $http.get()
+      //   .then(function (response) {
+      //       return response.data;
+      //   },
+      //   function (response) {
+      //       console.log('error');
+      //       console.log(response)
+      //   });
+      return ['Math', 'Science', 'History'];
+    }
   }
 })();
