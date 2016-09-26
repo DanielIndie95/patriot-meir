24045dba9bd41d8b45e162837185ab7a833d7e80
Ilan Khirin <ilan.khirin@gmail.com>
2016-09-26 10:36:16 +0300

    added add summary

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,19 +1,24 @@
 (function () {
-    angular.module('summarySharing')
-        .config(config);
+  angular.module('summarySharing')
+    .config(config);
 
-    function config($routeProvider) {
-        $routeProvider
-            .when('/viewSummary', {
-                templateUrl: 'pages/view-summary/view-summary.partial.html',
-                controller: 'viewSummaryCtrl',
-                controllerAs: 'vm'
-            })
-            .when('/main', {
-                templateUrl: 'pages/main-page/main-page.template.html',
-                controller: 'MainPageCtrl',
-                controllerAs: 'main'
-            })
-          .otherwise('/main');
-    }
+  function config($routeProvider) {
+    $routeProvider
+      .when('/viewSummary', {
+        templateUrl: 'pages/view-summary/view-summary.partial.html',
+        controller: 'viewSummaryCtrl',
+        controllerAs: 'vm'
+      })
+      .when('/main', {
+        templateUrl: 'pages/main-page/main-page.template.html',
+        controller: 'MainPageCtrl',
+        controllerAs: 'main'
+      })
+      .when('/newSummary', {
+        templateUrl: 'pages/new-summary-upload/new-summary-upload.partial.html',
+        controller: 'newSummaryUploadController',
+        controllerAs: 'newSummary'
+      })
+      .otherwise('/main');
+  }
 })();
