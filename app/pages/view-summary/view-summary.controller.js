59b3d900559def97f7ae0af3fac5315736684a8f
Meir Blachman <meirblachman@protonmail.com>
2016-09-26 11:37:51 +0300

    added comments to summary page

--- app/pages/view-summary/view-summary.controller.js
+++ app/pages/view-summary/view-summary.controller.js
@@ -8,66 +8,97 @@
   function ViewSummaryCtrl() {
     var vm = this;
 
-    vm.test = "hello world";
+    vm.questions = getQuestions();
+    vm.comments = getComments();
 
-    vm.questions = [
-      {
-        text: 'question 1',
-        answers: [
-          {
-            rating: {
-              count: 345,
-              value: 7.3
+    function getQuestions() {
+      return [
+        {
+          text: 'question 1',
+          answers: [
+            {
+              rating: {
+                count: 345,
+                value: 7.3
+              },
+              created: '19/06/2016',
+              text: 'answer 1.1'
             },
-            created: '19/06/2016',
-            text: 'answer 1.1'
+            {
+              rating: {
+                count: 234,
+                value: 6.3
+              },
+              created: '15/06/2016',
+              text: 'answer 1.2'
+            },
+            {
+              rating: {
+                count: 456,
+                value: 3.6
+              },
+              created: '13/06/2016',
+              text: 'answer 1.3'
+            }
+          ]
+        }, {
+          text: 'question 2',
+          answers: [
+            {
+              rating: {
+                count: 567,
+                value: 9.3
+              },
+              created: '19/06/2016',
+              text: 'answer 2.1'
+            },
+            {
+              rating: {
+                count: 433,
+                value: 6.1
+              },
+              created: '15/06/2016',
+              text: 'answer 2.2'
+            },
+            {
+              rating: {
+                count: 112,
+                value: 5.6
+              },
+              created: '13/06/2016',
+              text: 'answer 2.3'
+            }
+          ]
+        }
+      ];
+    }
+
+    function getComments() {
+      return [
+        {
+          user: {
+            name: 'Daniel',
+            id: 'abinav_t'
           },
-          {
-            rating: {
-              count: 234,
-              value: 6.3
-            },
-            created: '15/06/2016',
-            text: 'answer 1.2'
+          text: 'Vim nullam fastidii ei, nullam commune ei mei, pri te tation possim. Alii sensibus neglegentur cum an, corpora laboramus posidonium ei pro. Vero graecis vim an, meis tractatos conclusionemque sea no, mutat ludus cu vim. Cu sit diam aeterno, error civibus ei pro.',
+          created: '13/05/2016'
+        },
+        {
+          user: {
+            name: 'Yosi',
+            id: 'cheth'
           },
-          {
-            rating: {
-              count: 456,
-              value: 3.6
-            },
-            created: '13/06/2016',
-            text: 'answer 1.3'
-          }
-        ]
-      },{
-        text: 'question 2',
-        answers: [
-          {
-            rating: {
-              count: 567,
-              value: 9.3
-            },
-            created: '19/06/2016',
-            text: 'answer 2.1'
+          text: 'Vim nullam fastidii ei, nullam commune ei mei, pri te tation possim. Alii sensibus neglegentur cum an, corpora laboramus posidonium ei pro. Vero graecis vim an, meis tractatos conclusionemque sea no, mutat ludus cu vim. Cu sit diam aeterno, error civibus ei pro.',
+          created: '17/05/2016'
+        },
+        {
+          user: {
+            name: 'Eyal Golan',
+            id: 'ritu'
           },
-          {
-            rating: {
-              count: 433,
-              value: 6.1
-            },
-            created: '15/06/2016',
-            text: 'answer 2.2'
-          },
-          {
-            rating: {
-              count: 112,
-              value: 5.6
-            },
-            created: '13/06/2016',
-            text: 'answer 2.3'
-          }
-        ]
-      }
-    ];
+          text: 'Vim nullam fastidii ei, nullam commune ei mei, pri te tation possim. Alii sensibus neglegentur cum an, corpora laboramus posidonium ei pro. Vero graecis vim an, meis tractatos conclusionemque sea no, mutat ludus cu vim. Cu sit diam aeterno, error civibus ei pro.',
+          created: '17/05/2016'
+        }];
+    }
   }
 })();
